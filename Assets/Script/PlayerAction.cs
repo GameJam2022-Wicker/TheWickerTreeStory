03caaa608a643da23c60337dda58f82df8d46d42
Jangmanbo <wkddpdnjs99@gmail.com>
2022-08-19 22:42:53 +0900

    add : ladder climb
    사다리 오르기/내리기

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    [SerializeField] private LayerMask platformLayerMask = default;
+    [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private float gravityScale;
     public float fallGravityMultiflier;
 
     public float maxSpeed;
     public float jumpPower;
+    public float ladderSpeed;
     public bool isHurting = false;
 
     Rigidbody2D rigid;
     bool facingRight = true;
-    bool isKnockback = false;
     bool isJumping = false;
+    bool isLadder;
+    bool isClimbing;
+
+    private GameObject ladder;  // 현재 플레이어가 타고 있는 사다리
 
     private CapsuleCollider2D capsuleCollider2D;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
     private Color materialTintColor;
-
-    //private float coyoteTime = 0.2f;
-    //private float coyoteTimeCounter;
+    private LayerMask ladderLayer, tileLayer, playerLayer;
 
     void Awake()
     {
@@ -31,6 +33,10 @@ public class PlayerAction : MonoBehaviour
         capsuleCollider2D = GetComponent<CapsuleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        ladderLayer = LayerMask.NameToLayer("Ladder");
+        tileLayer = LayerMask.NameToLayer("Tile");
+        playerLayer = LayerMask.NameToLayer("Player");
     }
 
 
@@ -38,28 +44,44 @@ public class PlayerAction : MonoBehaviour
     {
         if (IsGrounded())
         {
-            //coyoteTimeCounter = coyoteTime;
+            Physics2D.IgnoreLayerCollision(playerLayer, tileLayer, false);
             rigid.gravityScale = gravityScale;
         }
         else
         {
-            //coyoteTimeCounter -= Time.deltaTime;
             rigid.gravityScale = gravityScale * fallGravityMultiflier;
         }
 
+        if (isLadder)   // 사다리 앞에 있는 경우
+        {
+            rigid.gravityScale = 0; // 중력 X
+
+            // 수직 방향키 입력 감지
+            Vector3 horizontalMove = Vector3.zero;
+            if (Input.GetKey(KeyCode.UpArrow))
+                horizontalMove = Vector3.up;
+            else if (Input.GetKey(KeyCode.DownArrow))
+                horizontalMove = Vector3.down;
+
+            if (horizontalMove!= Vector3.zero)  // 수직 방향키 입력이 있다면
+            {
+                isClimbing = true;
+                transform.position = new Vector3(ladder.transform.position.x, transform.position.y, transform.position.z);  // 플레이어를 사다리 가운데로 정렬
+                transform.position += horizontalMove * ladderSpeed * Time.deltaTime;    // 플레이어 이동
+                Physics2D.IgnoreLayerCollision(playerLayer, tileLayer, true); // 타일과의 충돌 처리 X
+            }
+        }
+
         //Jump
-        //if (coyoteTimeCounter > 0f && Input.GetButtonDown("Jump"))
         if (Input.GetButtonDown("Jump"))
         {
             isJumping = true;
             rigid.velocity = Vector2.up * jumpPower;
-            //jumpTimeCounter = jumpTime;
         }
 
         if (Input.GetButtonUp("Jump") && rigid.velocity.y > 0f)
         {
             rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * 0.5f);
-            //coyoteTimeCounter = 0f;
         }
 
         //Stop Speed
@@ -80,7 +102,8 @@ public class PlayerAction : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed * (-1))
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);*/
 
-        if (!isKnockback)
+        // 사다리를 타고 있으면서 땅에 있지 않은 경우 수평 이동 불가능
+        if (!isClimbing || isClimbing && IsGrounded())
         {
             float moveInput = Input.GetAxisRaw("Horizontal");
 
@@ -97,6 +120,26 @@ public class PlayerAction : MonoBehaviour
         }
     }
 
+    // 사다리 진입
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == ladderLayer)
+        {
+            ladder = collision.gameObject;
+            isLadder = true;
+            capsuleCollider2D.size = new Vector2(capsuleCollider2D.size.x / 2, capsuleCollider2D.size.y);
+        }
+    }
+
+    // 사다리 나가기
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == ladderLayer)
+        {
+            isLadder = isClimbing = false;
+            capsuleCollider2D.size = new Vector2(capsuleCollider2D.size.x * 2, capsuleCollider2D.size.y);
+        }
+    }
 
 
     void Flip()
@@ -150,7 +193,6 @@ public class PlayerAction : MonoBehaviour
         rigid.AddForce(knockBack, ForceMode2D.Impulse);
         DamageFlash();
 
-        isKnockback = true;
 
         StartCoroutine(CoEnableDamage(0.5f, 1.5f));
     }
@@ -160,7 +202,6 @@ public class PlayerAction : MonoBehaviour
         if (isHurting)
         {
             yield return new WaitForSeconds(waitTime1);
-            isKnockback = false;
             yield return new WaitForSeconds(waitTime2);
             materialTintColor = new Color(1, 1, 1, 1f);
             spriteRenderer.material.SetColor("_Color", materialTintColor);
