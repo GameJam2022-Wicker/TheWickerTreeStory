4d821da193361a839400c36b2480cb07ae993874
Jangmanbo <wkddpdnjs99@gmail.com>
2022-08-21 07:08:02 +0900

    add : Game Over animation
    판넬 페이드아웃 + 텍스트 페이드인
    -> MainMenu scene으로 이동

--- Assets/Script/FadeManager.cs
+++ Assets/Script/FadeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,6 +9,8 @@ public class FadeManager : MonoBehaviour
 {
     [SerializeField]
     private Image fadePanel;
+    [SerializeField]
+    private TextMeshProUGUI text;
 
     private void Start()
     {
@@ -40,5 +43,23 @@ public class FadeManager : MonoBehaviour
 
         int currentSceneNumber = SceneManager.GetActiveScene().buildIndex;  // 현재 scene number
         SceneManager.LoadScene(++currentSceneNumber);   // scene 전환
+    }
+
+    public IEnumerator GameOverCoroutine()
+    {
+        float fadeCount = 0;    // 초기 알파값(검은 화면)
+        while (true)    // 알파값이 최대(1)가 될 때까지 반복
+        {
+            if (fadeCount >= 1) // 페이드아웃 종료
+                break;
+
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f); // 0.01초마다 어두워지게->1초 후 완전히 어두워짐
+            fadePanel.color = new Color(0, 0, 0, fadeCount);    // 알파값 조정
+            text.color = new Color(text.color.r, text.color.g, text.color.b, fadeCount);
+        }
+
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);   // 게임 메뉴로 이동
     }
 }
