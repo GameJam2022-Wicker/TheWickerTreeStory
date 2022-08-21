173e4b0fdf6338f4bfea0bbaf90f83cab6395cc8
YesMan <kaka0804@naver.com>
2022-08-21 11:39:17 +0900

    update: shadowMove class
    섀도우가 출발하는 시각 변경

--- Assets/Script/ShadowMove.cs
+++ Assets/Script/ShadowMove.cs
@@ -25,7 +25,7 @@ public class ShadowMove : MonoBehaviour
 
     IEnumerator CoChasePlayer()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(3f);
         canChase = true;
         animator.SetTrigger("isWalking");
     }
