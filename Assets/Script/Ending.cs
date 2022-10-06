b663367c67ca65231f220a001b7e4c5bb769c92b
Jangmanbo <wkddpdnjs99@gmail.com>
2022-10-07 02:41:20 +0900

    update : Ending cutscene
    Ending.cs
    - 믿음에 따라 Start함수에서 해피엔딩, 배드엔딩 타임라인 세팅
    
    Ending.unnity
    - 두번째 씬 시작까지 구현

--- Assets/Script/Ending.cs
+++ Assets/Script/Ending.cs
@@ -1,33 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class Ending : MonoBehaviour
 {
     bool isBelieving;
-    public CutScene cutScene;
-
-    public List<Sprite> realEndingList;
-    public List<Sprite> badEndingLIst;
 
     public AudioSource bgm;
-    public AudioClip realBGM, badBGM;
+    public AudioClip happyBGM, badBGM;
+
+    private PlayableDirector playableDirector;
+    [SerializeField] private TimelineAsset happyTimeline, badTimeline;
+
+    private void Awake()
+    {
+        // J : Timeline 오브젝트의 PlayableDirector 컴포넌트 가져오기
+        playableDirector=GameObject.Find("Timeline").GetComponent<PlayableDirector>();
+    }
 
     private void Start()
     {
-        isBelieving = DataManager.instance.isBelieving;
+        isBelieving = DataManager.instance.isBelieving; // 믿는다vs믿지않는다
 
         if(isBelieving)
         {
-            bgm.clip = realBGM;
-            cutScene.imageList = realEndingList;
-            cutScene.imageNum = realEndingList.Count;
+            bgm.clip = happyBGM;
+            playableDirector.playableAsset = happyTimeline;
         }
         else
         {
             bgm.clip = badBGM;
-            cutScene.imageList = badEndingLIst;
-            cutScene.imageNum = badEndingLIst.Count;
+            playableDirector.playableAsset = badTimeline;
         }
 
         bgm.Play();
