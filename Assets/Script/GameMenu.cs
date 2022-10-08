5f9645d97ee32a1a3f21a03e2df7e12003075e3a
YesMan <kaka0804@naver.com>
2022-10-09 00:49:18 +0900

    update: temporary game menu (prototype)
    - 게임메뉴 버튼들 임시로 생성(start, credit, exit)
    - 버튼 위치, 크레딧 상세 기획 보고 수정

--- Assets/Script/GameMenu.cs
+++ Assets/Script/GameMenu.cs
@@ -1,12 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
-    public void ClickGameStart()
+    //버튼 누를 시 꺼짐
+    public GameObject buttonGroupObject;
+
+    //startButton 누를 시 켜고 꺼지는 오브젝트
+    public GameObject prologueFirstTextObject;
+    public GameObject menu_ImageObject;
+
+    //creditButton 누를 시 켜고 꺼지는 오브젝트
+    public GameObject credit_Object;
+
+    public Button start_Button;
+    public Button credit_Button;
+    public Button exit_Button;
+    public Button tempback;
+
+    private void Start()
     {
-        SceneManager.LoadScene(1);
+        start_Button.onClick.AddListener(OnClickStartButton);
+        credit_Button.onClick.AddListener(OnClickCreditButton);
+        exit_Button.onClick.AddListener(OnClickExitButton);
+        tempback.onClick.AddListener(OnClickBackButton);
+    }
+
+    public void OnClickStartButton()
+    {
+        prologueFirstTextObject.SetActive(true);
+        menu_ImageObject.SetActive(false);
+        buttonGroupObject.SetActive(false);
+    }
+
+    public void OnClickCreditButton()
+    {
+        buttonGroupObject.SetActive(false);
+        credit_Object.SetActive(true);
+        //임시
+        tempback.gameObject.SetActive(true);
+    }
+
+    //임시
+    public void OnClickBackButton()
+    {
+        credit_Object.SetActive(false);
+        buttonGroupObject.SetActive(true);
+        tempback.gameObject.SetActive(false);
+    }
+
+    public void OnClickExitButton()
+    {
+        Application.Quit();
     }
 }
